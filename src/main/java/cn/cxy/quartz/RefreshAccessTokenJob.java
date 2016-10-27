37289d8ef8ece5fed74cdf449b5a9e6ef6878ade
cx_yang <yangcx@thinkingbar.cn>
2016-10-27 23:06:11 +0800

    微信普通access_token的保存

--- src/main/java/cn/cxy/quartz/RefreshAccessTokenJob.java
+++ src/main/java/cn/cxy/quartz/RefreshAccessTokenJob.java
@@ -4,6 +4,7 @@ import cn.cxy.entity.AccessToken;
 import cn.cxy.util.JsonUtil;
 import cn.cxy.value.InitConfig;
 import cn.cxy.value.WeChatUrl;
+import cn.cxy.value.WeixinContext;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -26,20 +27,34 @@ public class RefreshAccessTokenJob extends QuartzJobBean {
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
         String format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
         System.err.println("-------开始获取普通Accesstoken："+ format +"-------");
+        CloseableHttpClient httpClient = null;
+        CloseableHttpResponse response = null;
         try {
-            CloseableHttpClient httpClient = HttpClients.createDefault();
+            httpClient = HttpClients.createDefault();
             String uri = WeChatUrl.NORMAL_ACCESS_TOKEN_URL.replace("APPID", InitConfig.APP_ID).replace("APPSECRET",InitConfig.APP_SECRET);
             HttpGet httpGet = new HttpGet(uri);
-            CloseableHttpResponse response = httpClient.execute(httpGet);
+            response = httpClient.execute(httpGet);
             int code = response.getStatusLine().getStatusCode();
             if (code == 200){
                 HttpEntity entity = response.getEntity();
                 String s = EntityUtils.toString(entity);
                 AccessToken accessToken = JsonUtil.deserialize(s, AccessToken.class);
                 System.err.println(accessToken);
+                WeixinContext.setAccesstoken(accessToken.getAccess_token());
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }finally {
+            try {
+                if (null != response){
+                    response.close();
+                }
+                if (null != httpClient){
+                    httpClient.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         System.out.println("-------获取普通Accesstoken结束-------");
     }
---
+++ src/main/java/cn/cxy/servlet/InitServlet.java
@@ -0,0 +1,24 @@
+package cn.cxy.servlet;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created by lenovo on 2016/10/27.
+ */
+public class InitServlet extends HttpServlet {
+
+    private static WebApplicationContext context;
+    private static String realPaht;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        context = WebApplicationContextUtils.getWebApplicationContext(this.getServletContext());
+        BeanFactoryContext.setContext(context);
+    }
+}
