ed6c570984e1034601442a5568efa62af3b04f93
Fabulous <yangchengxuan1991@163.com>
2016-11-23 11:04:13 +0800

    Update InitController.java
    merge tools

--- src/main/java/cn/cxy/controller/InitController.java
+++ src/main/java/cn/cxy/controller/InitController.java
@@ -68,8 +68,7 @@ public class InitController {
             Map<String, String> map = MsgUtils.req2Map(request);
             String s = MsgUtils.msgHandle(map);
             //TODO 请求头部信息
-            //测试消息
-            //冲突消息
+            ////how to use the eclipse's egit - merge tools
             response.setContentType("application/xml;charset=UTF-8");
             response.setCharacterEncoding("UTF-8");
             response.getWriter().write(s);
