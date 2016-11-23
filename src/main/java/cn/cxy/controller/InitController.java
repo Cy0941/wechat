90cde528e276fa0f358f2ce5358f55bee4664e2b
Fabulous <yangchengxuan1991@163.com>
2016-11-23 10:41:36 +0800

    Update InitController.java
    测试

--- src/main/java/cn/cxy/controller/InitController.java
+++ src/main/java/cn/cxy/controller/InitController.java
@@ -68,7 +68,6 @@ public class InitController {
             Map<String, String> map = MsgUtils.req2Map(request);
             String s = MsgUtils.msgHandle(map);
             //TODO 请求头部信息
-            //测试消息
             //冲突消息
             response.setContentType("application/xml;charset=UTF-8");
             response.setCharacterEncoding("UTF-8");
